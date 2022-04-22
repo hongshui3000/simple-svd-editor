28cf9c2cbc6f01cd4ea66ee728f70c158647f516
Ilya Yurchenko <ilyha19@live.ru>
2022-04-23 01:16:21 +0300

    add port to pipeline

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
     apps: [
         {
-            script: isProd ? 'yarn start' : './',
+            script: isProd ? `yarn start -- -p ${process.env.PORT || 3000}` : './',
             name: 'svd-editor',
             max_restarts: 2,
             restart_delay: 2000,
